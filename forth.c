8977a931afcd6eb9e869f468208c0c58ddb4e389
Tehologist <todd.b.thomas@gmail.com>
2017-05-05 10:45:05 -0400

    Updated source to remove extraneous code.

--- forth.c
+++ forth.c
@@ -23,30 +23,19 @@
 #define INTERPRETER 0
 #define WORDLIST    2
 #define IMMEDIATE   3
-struct FILE_HANDLE {
-    int handle_set;
-    FILE* handle;
-};
 struct IMAGE {
     char memory[VM_SIZE];
-    struct FILE_HANDLE file_handles[NUM_FILE_HANDLES];
+    FILE* file_handles[NUM_FILE_HANDLES];
 };
-unsigned short create_handle(struct FILE_HANDLE * fh, FILE* handle) {
+unsigned short create_handle(FILE** fh, FILE* handle) {
     int index = 0;
     while(index++ < NUM_FILE_HANDLES) {
-        if(fh[index].handle_set == FALSE) {
-			fh[index].handle_set = TRUE;
-            fh[index].handle = handle;
+        if(fh[index] == NULL) {
+            fh[index] = handle;
             return index;
         }
     }
     return 0;
-}
-void vm_pokeByte(struct IMAGE * image, int position, unsigned char value) {
-    image->memory[position] = value;
-}
-unsigned char vm_peekByte(struct IMAGE * image, int position) {
-    return image->memory[position];
 }
 #define GET(X) *(unsigned short*)(image->memory + X)
 #define SET(X,Y) *(unsigned short *)(image->memory + X) = Y
@@ -182,12 +171,12 @@ int vm_run(struct IMAGE * image, int start) {
         case 18: // TX!
             POPDS(regA);
             POPDS(regB);
-			fputc(regB, image->file_handles[regA].handle);
+            fputc(regB, image->file_handles[regA]);
             NEXT;
             break;
         case 19: // ?RX
             POPDS(regA);
-			regA = fgetc(image->file_handles[regA].handle);
+            regA = fgetc(image->file_handles[regA]);
             PUSHDS(regA);
             PUSHDS(TRUE);
             NEXT;
@@ -200,26 +189,26 @@ int vm_run(struct IMAGE * image, int start) {
                 POPDS(regA);
                 vm_tostring(image, regA, filename);
                 vm_tostring(image, regB, options);
-				PUSHDS(create_handle(image->file_handles, fopen(filename,options)));
+                PUSHDS(create_handle(image->file_handles, fopen(filename,options)));
             }
             NEXT;
             break;
         case 21: // F_CLOSE
             POPDS(regA);
-            fclose(image->file_handles[regA].handle);
-            image->file_handles[regA].handle_set = FALSE;
+            fclose(image->file_handles[regA]);
+            image->file_handles[regA] = NULL;
             NEXT;
             break;
         case 22: // C@
             POPDS(regA);
-			regB  = image->memory[regA];
+            regB  = image->memory[regA];
             PUSHDS(regB);
             NEXT;
             break;
         case 23: // C!
             POPDS(regA);
             POPDS(regB);
-			image->memory[regA] = regB;
+            image->memory[regA] = regB;
             NEXT;
             break;
         default: // CALL
@@ -236,60 +225,44 @@ int vm_run(struct IMAGE * image, int start) {
 void vm_init(struct IMAGE * image, FILE * fi, int size) {
     int x = 0;
     while(x < NUM_FILE_HANDLES) {
-        image->file_handles[x].handle_set = FALSE;
-		x++;
+        image->file_handles[x++] = NULL;
     }
-    image->file_handles[0].handle_set = TRUE;
-    image->file_handles[0].handle = stdin;
-    image->file_handles[1].handle_set = TRUE;
-    image->file_handles[1].handle = stdout;
+    image->file_handles[0] = stdin;
+    image->file_handles[1] = stdout;
     x = 0;
     while(x < VM_SIZE) {
-		image->memory[x++] = (fi == NULL ? 0 : fgetc(fi));
+        image->memory[x++] = (fi == NULL ? 0 : fgetc(fi));
     }
     SET(SP0, 35 * CELL_SIZE);
     SET(SPP, 35 * CELL_SIZE);
     SET(RP0, 99 * CELL_SIZE);
     SET(RPP, 99 * CELL_SIZE);
 }
-void int_poke(struct IMAGE * image, int position, unsigned short value) {
-	vm_pokeByte(image, position + 1, (value >> 8) & 0xFF);
-	vm_pokeByte(image, position, (value & 0xFF));
-}
-unsigned short int_peek(struct IMAGE * image, int position) {
-		unsigned char byte1 = vm_peekByte(image, position);
-		unsigned char byte2 = vm_peekByte(image, position + 1);
-		return ((byte2 << 8) | byte1);
-}
-int  int_getCompiler(struct IMAGE * image) { return int_peek(image, STATE); }
-void int_setcompiler(struct IMAGE * image, int val) {
-    int_poke(image, STATE, val); }
+int  int_getCompiler(struct IMAGE * image) { return GET(STATE); }
+void int_setcompiler(struct IMAGE * image, int val) { SET(STATE, val); }
 void int_create(struct IMAGE * image, char * word, int size,
     unsigned short type, unsigned short data) {
     int x = 0;
     int charstart;
-    int position = int_peek(image, NP);
+    int position = GET(NP);
     position = position - size;
     charstart = position + 1;
-    vm_pokeByte(image, position, size);
-    int_poke(image, position - (3 * CELL_SIZE), int_peek(image, LAST));
-    int_poke(image, LAST, position);
-    while(x < size) {
-		vm_pokeByte(image, charstart + x, word[x]);
-		x = x + 1;
-    }
-    if((size) % 2 == 1) vm_pokeByte(image, charstart + size, '\0');
-    int_poke(image, position - (1 * CELL_SIZE), type);
-    int_poke(image, position - (2 * CELL_SIZE), data);
-    int_poke(image, NP, position - (4 * CELL_SIZE));
+    image->memory[position] = size;
+    SET(position - (3 * CELL_SIZE), GET(LAST));
+    SET(LAST, position);
+    while(x < size) { image->memory[charstart + x] = word[x];x++; }
+    if((size) % 2 == 1) image->memory[charstart + size] = '\0';
+    SET(position - (1 * CELL_SIZE), type);
+    SET(position - (2 * CELL_SIZE), data);
+    SET(NP, position - (4 * CELL_SIZE));
 }
 void int_init(struct IMAGE * image) {
-    int_poke(image, NP,      (VM_SIZE / 4) - 1);
-    int_poke(image, CP,      (182 * CELL_SIZE));
-    int_poke(image, NUM_TIB, (100 * CELL_SIZE));
-    int_poke(image, TIB,     ((100 * CELL_SIZE) + 1));
-    int_poke(image, CURRENT, FORTH);
-    int_poke(image, CONTEXT, FORTH);
+    SET(NP,      (VM_SIZE / 4) - 1);
+    SET(CP,      (182 * CELL_SIZE));
+    SET(NUM_TIB, (100 * CELL_SIZE));
+    SET(TIB,     ((100 * CELL_SIZE) + 1));
+    SET(CURRENT, FORTH);
+    SET(CONTEXT, FORTH);
     int_create(image, ":",       1, INTERPRETER, 41);
     int_create(image, ";",       1, INTERPRETER, 42);
     int_create(image, "NOP",     3, WORDLIST,     0);
@@ -316,15 +289,15 @@ void int_init(struct IMAGE * image) {
     int_create(image, "F_CLOSE", 7, WORDLIST,    21);
     int_create(image, "C@",      2, WORDLIST,    22);
     int_create(image, "C!",      2, WORDLIST,    23);
-    int_poke(image, int_peek(image, CURRENT), int_peek(image,LAST));
+    SET(GET(CURRENT), GET(LAST));
 }
 int int_word(struct IMAGE * image, char * word) {
-    int numIn = int_peek(image,TO_IN);
-    int length = vm_peekByte(image,int_peek(image, NUM_TIB));
+    int numIn = GET(TO_IN);
+    int length = image->memory[GET(NUM_TIB)];
     int start = numIn;
     int count = 0;
     while(numIn < length) {
-        char c = (char)vm_peekByte(image, int_peek(image, TIB) + numIn);
+        char c = (char)image->memory[GET(TIB) + numIn];
         numIn = numIn + 1;
         if(c == ' ') {
             word[count] = '\0';
@@ -333,62 +306,55 @@ int int_word(struct IMAGE * image, char * word) {
         else word[count] = c;
         count = count + 1;
     }
-    int_poke(image, TO_IN, numIn);
+    SET(TO_IN, numIn);
     return numIn - start;
 }
 int int_find(struct IMAGE * image, char * word, int size) {
-    int start = int_peek(image, int_peek(image, CONTEXT));
-    int len   = vm_peekByte(image, start);
+    int start = GET(GET(CONTEXT));
+    int len   = image->memory[start];
     while(start != 0) {
         if(len == size) {
             int match = TRUE;
             int x = 0;
-			while ( x < len) {
-				match &= (word[x] == vm_peekByte(image, start + 1 + x));
-				x = x + 1;
-			}
-            if(match > FALSE)
-                return start;
+            while ( x < len) { 
+                match &= (word[x] == image->memory[start + 1 + x]);x++; }
+            if(match > FALSE) return start;
         }
-        start = int_peek(image, start - (3 * CELL_SIZE));
-        len   = vm_peekByte(image, start);
+        start = GET(start - (3 * CELL_SIZE));
+        len   = image->memory[start];
     }
     return 0;
 }
 int int_number(int * val, char * word, int size) {
-    int ndx = 0;
-    int negative = 0;
-    if(size == 0)
-        return 0;
+    int ndx, negative = 0;
+    if(size == 0) return 0;
     if(word[ndx] == '-') {
-        negative = 1;
-        ndx = ndx + 1;
+	    negative = 1;
+	    ndx++;
     }
     while(ndx < size) {
         if(word[ndx] >= '0' && word[ndx] <= '9')
             *val = ((*val * 10) + (word[ndx] - '0'));
-        else
-            return 0;
-        ndx = ndx + 1;
+        else return 0;
+        ndx++;
     }
-    if(negative == 1)
-        *val = ~*val + 1;
+    if(negative == 1) { *val = ~*val + 1; }
     return 1;
 }
-#define HERE int_peek(image, CP)
+#define HERE GET(CP)
 void int_comma(struct IMAGE * image, unsigned short val) {
-    int_poke(image, HERE, val);
-    int_poke(image, CP, HERE + CELL_SIZE);
+    SET(HERE, val);
+    SET(CP, HERE + CELL_SIZE);
 }
 int int_interpret(struct IMAGE * image) {
     int sz, found, num, type, data;
     char word[128];
-    while(int_peek(image,TO_IN) < vm_peekByte(image,int_peek(image, NUM_TIB))) {
+    while(GET(TO_IN) < image->memory[GET(NUM_TIB)]) {
         sz = int_word(image, word);
         found = int_find(image, word, sz-1);
         if(found != FALSE) {
-            type = int_peek(image, found - (1 * CELL_SIZE));
-            data = int_peek(image, found - (2 * CELL_SIZE));
+            type = GET(found - (1 * CELL_SIZE));
+            data = GET(found - (2 * CELL_SIZE));
             if(type == INTERPRETER) {
                 switch(data) {
                 case 41: // :
@@ -399,7 +365,7 @@ int int_interpret(struct IMAGE * image) {
                 case 42: // ;
                     int_comma(image, EXIT);
                     int_setcompiler(image, FALSE);
-                    int_poke(image, int_peek(image, CURRENT), int_peek(image, LAST));
+                    SET(GET(CURRENT), GET(LAST));
                     break;
                 default:
                     break;
@@ -407,53 +373,44 @@ int int_interpret(struct IMAGE * image) {
             }
             else if(type == IMMEDIATE ||
                 (type == WORDLIST && int_getCompiler(image) == FALSE)) {
-                if(vm_run(image, data) == -1)
-                    return -1;
+                if(vm_run(image, data) == -1) return -1;
             }
-            else {
-                int_comma(image, data);
-            }
+            else int_comma(image, data);
         }
         else {
             num = 0;
-            if(int_number(&num,word,sz - 1)) {
+            if(int_number(&num, word, sz - 1)) {
                 if(int_getCompiler(image) != FALSE) {
                     int_comma(image, LIT);
                     int_comma(image, num);
                 }
             }
             else {
-                if(word[0] != 0 && word[0] != '\n' && word[0] != '\r') {
+                if(word[0] != 0 && word[0] != '\n' && word[0] != '\r') 
                     word[sz] = '\0';
-                }
             }
         }
     }
     return 1;
 }
 int int_readfile(FILE * fp, struct IMAGE * image) {
-	char c = 1;
-	int i;
-	while(TRUE) {
-		i = 0;
-		while(c != '\n') {
-			c = fgetc(fp);
-			vm_pokeByte(image, int_peek(image, TIB) + i, c);
-			i++;
-			if(c == EOF) return -1;
-		}
-		vm_pokeByte(image, int_peek(image, NUM_TIB),i);
-	    int_poke(image, TO_IN, 0);
-		return i - 1;
-	}
-}
-int int_eval(struct IMAGE * image, FILE * fi) {
-    while(int_readfile(fi, image) != -1) {
-        if(int_interpret(image) == -1) {
-            return -1;
+    char c = 1;
+    int i;
+    while(TRUE) {
+        i = 0;
+        while(c != '\n') {
+            c = fgetc(fp);
+            image->memory[GET(TIB) + i++] = c;
+            if(c == EOF) return -1;
         }
+	image->memory[GET(NUM_TIB)] = i;
+        SET(TO_IN, 0);
+	return i - 1;
     }
-    return 0;
+}
+void int_eval(struct IMAGE * image, FILE * fi) {
+    while(int_readfile(fi, image) != -1) 
+        int_interpret(image);
 }
 struct IMAGE image;
 int main(int argc, char *argv[]) {
